6e8a27712e3e4c77db99f98cac3f97b3b448108e
westos <westos@westos.local>
2017-01-17 23:47:12 +0800

    add db user passwd

--- handlers/index.py
+++ handlers/index.py
@@ -8,6 +8,7 @@ Created on 2017��1��17��
 
 import tornado.web
 
+from methods import readdb
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
@@ -16,6 +17,15 @@ class IndexHandler(tornado.web.RequestHandler):
     def post(self):
         username = self.get_argument("username")
         password = self.get_argument("password")
+
+        info = readdb.select_info("myapp", "*", "username", username)
+
+        if info:
+            passwd = info[0][2]
+            if password == passwd:
+                self.write("welcome you: " + username)
+            else:
+                self.write("your password are not right")
+        else:
+            self.write("There is no the " + username)
         
-        self.write(username)
-       
--- methods/db.py
+++ methods/db.py
@@ -6,8 +6,8 @@ Created on 2017��1��17��
 '''
 import MySQLdb
 
-conn = MySQLdb.connect(host="10.218.139.81", user="root", passwd="westos", db="myapp", port=3306, charset="utf8")   #连接对象
-
+#conn = MySQLdb.connect(host="10.218.139.81", user="root", passwd="westos", db="myapp", port=3306, charset="utf8")   #连接对象
+conn = MySQLdb.connect(host="localhost", user="root", passwd="westos", db="westos", port=3306, charset="utf8")
 cur = conn.cursor()    #游标对象
 
 
